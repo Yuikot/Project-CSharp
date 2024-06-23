fc30d5d2d2a2af036a6bed21cbebf2e64bdb6ebe
Yuikot <119784063+Yuikot@users.noreply.github.com>
2024-06-23 17:18:52 +0200

    Update Program.cs

--- csharp project 3.2-4/Program.cs
+++ csharp project 3.2-4/Program.cs
@@ -23,11 +23,76 @@ void z2()
 
 void z3()
 {
-    //
-    //
-    //ASCII jest nieprawidłowe. Klucze DES są niemożliwe do stworzenia z 6 bajtów, musi być 8
-    //
-    //
+    string encryptedHex = "23c73dde8faedd91413fb5dd1d7e066d70425ed1e058d0e2f7e9e43501824a95446baf28f6ce7ffd3c544f40efb5c80f235de1321214328781a6ea0c0c4c7b74be3968ca1ffb8455";
+    byte[] encryptedBytes = HexStringToByteArray(encryptedHex);
+    string knownPlaintext = "test";
+
+    byte[] key = new byte[8];
+    key[2] = key[3] = key[4] = key[5] = (byte)'5';
+
+    key[6] = key[7] = 0;
+
+    Stopwatch stopwatch = Stopwatch.StartNew();
+
+    for (int i = 0; i <= 0xFFFF; i++)
+    {
+        key[0] = (byte)(i >> 8);
+        key[1] = (byte)i;
+
+        if (TryDecrypt(encryptedBytes, key, knownPlaintext))
+        {
+            stopwatch.Stop();
+            Console.WriteLine("Key found: " + BitConverter.ToString(key).Replace("-", ""));
+            Console.WriteLine("Time taken: " + stopwatch.Elapsed);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Key not found: " + BitConverter.ToString(key).Replace("-", ""));
+        }
+    }
+
+
+    static bool TryDecrypt(byte[] encryptedBytes, byte[] key, string knownPlaintext)
+    {
+        using (var des = DES.Create())
+        {
+            des.Key = key;
+            des.Mode = CipherMode.ECB;
+            des.Padding = PaddingMode.None;
+
+            try
+            {
+                using (var decryptor = des.CreateDecryptor())
+                {
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                    string decryptedText = Encoding.ASCII.GetString(decryptedBytes);
+
+                    if (decryptedText.StartsWith(knownPlaintext))
+                    {
+                        Console.WriteLine("Decrypted text: " + decryptedText);
+                        return true;
+                    }
+                }
+            }
+            catch (CryptographicException)
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    static byte[] HexStringToByteArray(string hex)
+    {
+        int numberChars = hex.Length;
+        byte[] bytes = new byte[numberChars / 2];
+        for (int i = 0; i < numberChars; i += 2)
+        {
+            bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        }
+        return bytes;
+    }
 
 }
 
